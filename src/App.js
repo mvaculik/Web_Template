528384e7ff8d898b0c5e89b5a52af59759bf6573
mvaculik <matyvaculik@gmail.com>
2021-07-22 18:35:38 +0200

    add footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 // Components
 import Header from './components/header';
-import Bottom from './components/bottom';
+// import Bottom from './components/bottom';
 import Parallax from "./components/parallax";
 
 // Pages
@@ -20,7 +20,9 @@ import ParallaxImg from "./img/background-px.jpg";
 import Map from "./img/map.PNG";
 
 // Icon
-
+import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { SiTripadvisor } from "react-icons/si";
 
 
 
@@ -31,7 +33,6 @@ function App() {
 
       <ParallaxProvider>
         <Header/>
-        <Bottom/>
 
         <div className="parallax">
           <Parallax imgsrc={ParallaxImg}></Parallax>
@@ -55,26 +56,31 @@ function App() {
           <img src={Logo} />
           <div className="row-footer">
             <div class="column-footer">
-              <p>SOCIAL SITES</p>
+              <h3>SOCIAL SITES</h3>
               <p>#</p>
               <p>#</p>
             </div>
             <div class="column-footer">
-              <p>OPENING HOURS</p>
+              <h3>OPENING HOURS</h3>
               <p>#</p>
               <p>#</p>
             </div>
             <div class="column-footer">
-            <p>CONTACTS</p>
+            <h3>CONTACTS</h3>
             <p>#</p>
             <p>#</p>
             </div>
+          <section>
+            <a><i><FaFacebookF/></i></a>
+            <a><i><FaInstagram/></i></a>
+            <a><i><SiTripadvisor/></i></a>
+          </section>
           </div>
+        <div className="bottom">
+        <p>Copyright © 2021 - 2022 NOLIMITS.</p>
+        </div>
         </div>
 
-        <div className="copyright">
-
-        </div>
 
     </div>
   );
